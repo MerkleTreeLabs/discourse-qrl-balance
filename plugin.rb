b2a9b81b09abd78cc2e8a392ddd45a82307bc5b7
fr1t2 <fr1t2@protonmail.com>
2025-08-27 21:37:49 -0600

    Moving error prone NoMethodError to settings file

--- plugin.rb
+++ plugin.rb
@@ -4,35 +4,10 @@
 # about: Displays QRL address balance from the QRL Explorer API on Discourse topic pages.
 # version: 0.1.0
 # authors: Your Name/AI
-# url: https://github.com/your-username/discourse-qrl-address-balance
+# url: https://github.com/your-username/discourse-qrl-address-balance [2]
 
 # General asset registration
 register_asset 'stylesheets/common/qrl-address-balance.scss'
-
-# --- NEW APPROACH FOR ADDING SITE SETTINGS ---
-# Define plugin settings using DiscoursePluginRegistry.
-# This is a robust way to add settings and is often used by core plugins.
-# These calls are placed directly at the top level of plugin.rb.
-DiscoursePluginRegistry.add_plugin_setting(:qrl_address_balance_enabled, {
-  default: true,
-  type: :boolean,
-  description: "Enable the QRL Address Balance plugin."
-})
-DiscoursePluginRegistry.add_plugin_setting(:qrl_explorer_base_url, {
-  default: "https://explorer.theqrl.org",
-  type: :string,
-  description: "Enter the base URL for the QRL Explorer API. E.g., https://explorer.theqrl.org"
-})
-DiscoursePluginRegistry.add_plugin_setting(:qrl_category_id, {
-  type: :category_id,
-  description: "Select the Discourse category where QRL address balances should display."
-})
-DiscoursePluginRegistry.add_plugin_setting(:qrl_balance_refresh_interval_minutes, {
-  default: 5,
-  type: :integer,
-  min: 1,
-  description: "How often to refresh the QRL balance data on the client side (in minutes)."
-})
 
 # The `after_initialize` block contains all other plugin logic.
 after_initialize do
@@ -43,6 +18,7 @@ after_initialize do
   end
 
   # --- Conditional Execution based on Plugin Enablement ---
+  # SiteSetting variables are automatically loaded from settings.yml now.
   return unless SiteSetting.qrl_address_balance_enabled
 
   # --- Register a custom topic field ---
